8d4590018296acbb54253e53b449ceedb30a7988
Samudhbhav <samudhbhavpr@gmail.com>
2015-05-22 12:25:55 -0700

    Samudhbhav/pythonds/basic/stack1.py
    Used in Prg4.py

---
+++ Samudhbhav/stack1.py
@@ -0,0 +1,24 @@
+# Samudhbhav/pythonds/basic/stack1.py
+# Contents of stack1.py :
+# Used in Prg4.py
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+        
+    def push(self, item):
+        self.items.append(item)
+
+    def insert(self,index, item):
+        self.items.insert(index,item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
